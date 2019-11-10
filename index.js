75406ac110082f6fa3046d4c93d94a9c203082a5
heliumface770 <heliumface770@gmail.com>
2019-11-09 18:45:42 -0700

    found the correct queryURL

--- index.js
+++ index.js
@@ -3,31 +3,34 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 
-    inquirer.prompt([  
-    {type: "input",
-    message: "What's your GitHub username?",
-    name: "username"},
-    {type: "input",
-    message: "What's your favorite color?",
-    name: "color"}
+inquirer.prompt([  
+    // {type: "input",
+    // message: "What's your GitHub username?",
+    // name: "username"},
+    // {type: "input",
+    // message: "What's your favorite color?",
+    // name: "color"}
 ])
 
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+.then(function({ username }) {                     //${username}
+    const queryUrl = `https://api.github.com/users/heliumface770`;
 
     axios.get(queryUrl).then(function(res) {
+        console.log(res);
       const repoNames = res.data.map(function(repo) {
+          //data is array that contains all repos
         return repo.name;
       });
 
-      const repoNamesStr = repoNames.join("\n");
+    //   const repoNamesStr = repoNames.join("\n");
 
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
+    //   fs.writeFile("repos.txt", repoNamesStr, function(err) {
+    //     if (err) {
+    //       throw err;
+    //     }
 
-        console.log(`Saved ${repoNames.length} repos`);
+        // console.log(`Saved ${repoNames.length} repos`);
+        console.log(res)
       });
     });
-  });
+  
