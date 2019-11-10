2f16f5a40f9fc4de7f80ee6c8b2b4617ac6fff6c
heliumface770 <heliumface770@gmail.com>
2019-11-10 12:41:05 -0700

    pdf styling -avatar, git icon, location

--- generateHTML.js
+++ generateHTML.js
@@ -1,31 +1,4 @@
-// const colors = {
-//     green: {
-//       wrapperBackground: "#E6E1C3",
-//       headerBackground: "#C1C72C",
-//       headerColor: "black",
-//       photoBorderColor: "#black"
-//     },
-//     blue: {
-//       wrapperBackground: "#5F64D3",
-//       headerBackground: "#26175A",
-//       headerColor: "white",
-//       photoBorderColor: "#73448C"
-//     },
-//     pink: {
-//       wrapperBackground: "#879CDF",
-//       headerBackground: "#FF8374",
-//       headerColor: "white",
-//       photoBorderColor: "#FEE24C"
-//     },
-//     red: {
-//       wrapperBackground: "#DE9967",
-//       headerBackground: "#870603",
-//       headerColor: "white",
-//       photoBorderColor: "white"
-//     }
-//   };
-
-  const colors = {
+const colors = {
     red: {
 
     },
@@ -51,6 +24,7 @@
 
     }}
   
+
 var html = 
 `<!DOCTYPE html>
 <html lang="en">
@@ -67,7 +41,6 @@ var html =
         top: 2%;
         left: 25%;
       }
-
       #avatarJPG {
         position: relative;
         float: left;
@@ -78,32 +51,27 @@ var html =
         bottom: 20px;
         background: #00abe7;
       }
-
       #github-icon {
         position: absolute;
         width: 8%;
         top: 214px;
         right: 740px;
       }
-
       #loc-icon {
         position: absolute;
         right: 100px;
         left: 23%;
       }
-
       #loc {
         position: absolute;
         top: 234px;
         left: 130px;
         font-size: 30px;
       }
-
       #name {
         position: relative;
         font-size: 50px;
       }
-
       #ref {
         position: absolute;
         float: left;
@@ -113,13 +81,12 @@ var html =
       }
     </style>
   </head>
-
   <body>
     <div id="avatar-container">
       <div id="avatar">
         <img
           id="avatarJPG"
-          src="https://avatars0.githubusercontent.com/u/49894687?v=4"
+          src="./"
         />
         <img
           class="icon"
@@ -131,14 +98,11 @@ var html =
         <div id="name">heliumface770</div>
       </div>
     </div>
-
     <!-- <img id="ref" src="./images/design.png" /> -->
-
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="./index.js"></script>
   </body>
 </html>
 `
-
 
 module.exports = html;
--- index.js
+++ index.js
@@ -1,50 +1,136 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const pdf = require('html-pdf');
-var html = require("./generateHTML")
-
+const pdf = require("html-pdf");
+// var html = require("./generateHTML")
 // var html = fs.readFileSync('./index.html', 'utf8');
-var options = { format: 'Letter' };
+var options = { format: "Letter" };
 
- 
-pdf.create(html, options).toFile('./test2.pdf', function(err, res) {
-  if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
-});
-
-
-inquirer.prompt([  
+inquirer
+  .prompt([
     // {type: "input",
     // message: "What's your GitHub username?",
     // name: "username"},
     // {type: "input",
     // message: "What's your favorite color?",
     // name: "color"}
-])
+  ])
 
-.then(function({ username }) {                     //${username}
+  .then(function({ username }) {
+    //${username}
     const queryUrl = `https://api.github.com/users/heliumface770`;
 
     axios.get(queryUrl).then(function(res) {
-        html(res);
+      html(res);
     });
-});
+  });
 
-function html(res){
-    const x = res.data;
-    const avatar =  x.avatar_url;
-    const login = x.login;
-    const location = x.location;
-    const htmlURL = x.html_url;
-    const blog = x.blog;
-    const bio = x.bio;
-    const repos = x.public_repos;
-    const followers = x.followers;
-    const following = x.following;
-    const name = x.name
+function html(res) {
+  const x = res.data;
+  const avatar = x.avatar_url;
+  const login = x.login;
+  const location = x.location;
+  const htmlURL = x.html_url;
+  const blog = x.blog;
+  const bio = x.bio;
+  const repos = x.public_repos;
+  const followers = x.followers;
+  const following = x.following;
+  const name = x.name;
+  console.log(avatar);
 
-    }
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+  <style>
+      body {
+        font-family: 'Oswald', sans-serif;
+      }
 
- 
-  
+      .pageTxt {
+          position: absolute;
+          font-size: 300%;
+          top: 2%;
+          left: 25%;
+        }
+        #avatarJPG {
+            position: relative;
+            float: left;
+            border-radius: 50%;
+            width: 27%;
+            padding: 4%;
+            right: 20px;
+            bottom: 20px;
+            background: #00abe7;
+        }
+        #github-icon {
+            position: absolute;
+            width: 8%;
+            top: 214px;
+            right: 740px;
+        }
+        #loc-icon {
+            position: absolute;
+            width: 20px;
+            right: 100px;
+            left: 22%;
+            top: 220px;
+        }
+        #loc {
+            position: absolute;
+            top: 224px;
+            left: 130px;
+            font-size: 30px;
+        }
+        #name {
+            position: absolute;
+            text-align: left;
+            left: -200px;
+            top: -15px;
+            font-size: 50px;
+            padding: 10px 500px;
+            background: #F8A447;
+            z-index: -10;
+        }
+        #ref {
+            position: absolute;
+            float: left;
+            top: 800px;
+            right: 20px;
+            width: 100%;
+        }
+        </style>
+        </head>
+        <body>
+        <div id="avatar-container">
+        <div id="avatar">
+        <img
+        id="avatarJPG"
+        src="${avatar}"
+        />
+        <img
+        class="icon"
+        id="github-icon"
+        src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-256.png"
+        />
+        <img id="loc-icon" src="https://cdn2.iconfinder.com/data/icons/pittogrammi/142/94-256.png" />
+        <div class="pageTxt" id="loc">${location}</div>
+        <div id="name">${login}</div>
+        </div>
+        </div>
+        <!-- <img id="ref" src="./images/design.png" /> -->
+        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+        <script src="./index.js"></script>
+        </body>
+        </html>
+        `;
+
+  pdf.create(html, options).toFile("./test2.pdf", function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
+}
