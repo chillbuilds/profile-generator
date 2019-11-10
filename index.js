2e69e7582b942d22c79affa15aed37c9bc3c01e1
heliumface770 <heliumface770@gmail.com>
2019-11-09 17:49:39 -0700

    multi question prompt bug fixed

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
+
+    inquirer.prompt([  
+    {type: "input",
+    message: "What's your GitHub username?",
+    name: "username"},
+    {type: "input",
+    message: "What's your favorite color?",
+    name: "color"}
+])
+
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
